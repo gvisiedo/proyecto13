a93bb1839d1359972688f3bd6517840fd1a01dff
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-02-09 08:57:55 +0100

    creando las clsaes

---
+++ js/Egreso.js
@@ -0,0 +1,10 @@
+class Egreso extends Dato{
+    static contadorEngresos = 0;
+    constructor(descripcion, valor){
+        super(descripcion, valor);
+        this._id = ++Egreso.contadorEngresos;
+    }
+    get id(){
+        return this._id;
+    }
+}
