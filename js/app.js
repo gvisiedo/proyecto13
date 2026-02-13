d1c1c964152dcd102823515f2484ae75650c11db
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-02-13 07:49:58 +0100

    last commit

--- js/app.js
+++ js/app.js
@@ -104,3 +104,20 @@ let eliminarEgreso = (id)=>{
     cargarCabecero();
     cargarEgresos();
 }
+let agregarDato = ()=>{
+    let forma = document.forms['forma'];
+    let tipo = forma['tipo'];
+    let descripcion = forma['decripcion'];
+    let valor = forma['valor'];
+    if(descripcion.value !='' && valor.value !=''){
+        if(tipo.value === 'ingreso'){
+            ingresos.push(new Ingreso(descripcion.value, Number(valor.value)));
+            cargarCabecero();
+            cargarIngresos();
+        }else if(tipo.value ==='egreso'){
+            egresos.push(new Egreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarEgresos();
+        }
+    }
+}
